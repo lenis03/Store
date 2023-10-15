728d850cc148be9bdf7714085c42aef0686ad5ef
lenis <fardintorkamand83@gmail.com>
2023-10-15 16:59:28 +0330

    Create category model & foreignkey to it

--- store/models.py
+++ store/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=500, blank=True)
+    datetime_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categorise'
+
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField()
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
