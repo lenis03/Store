9ae221b945762d3d9aefaeef10af852c2d50a547
lenis <fardintorkamand83@gmail.com>
2023-10-15 10:50:43 +0330

    Create Customer model

--- store/models.py
+++ store/models.py
@@ -9,3 +9,11 @@ class Product(models.Model):
     inventory = models.IntegerField()
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=11, unique=True)
+    birth_date = models.DateField()
