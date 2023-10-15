d28d57c9945b3bc1b0d8b6591cc04650912b817e
lenis <fardintorkamand83@gmail.com>
2023-10-15 10:30:09 +0330

    Create product model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+    
