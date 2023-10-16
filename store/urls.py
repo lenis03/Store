46d27a7655fe14b27615f76d163c37b6d1e4b7cd
lenis <fardintorkamand83@gmail.com>
2023-10-16 19:46:27 +0330

    Delete some not important code

--- store/urls.py
+++ store/urls.py
@@ -5,9 +5,4 @@ from store import views
 app_name = 'store'
 
 urlpatterns = [
-    path('', views.store, name='store'),
-    path('hello_world/', views.hello_world, name='hello_world'),
-    path('get_num/<int:num>/', views.get_num, name='get_num'),
-    path('hello_to_user/<str:name>/', views.say_hello_to_user, name='hello_to_user'),
-  
 ]
