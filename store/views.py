0a1b30d90f37035ee4d45027e446857b90236c25
lenis <fardintorkamand83@gmail.com>
2023-10-22 11:43:19 +0330

    Get list of products that were in one orderitems

--- store/views.py
+++ store/views.py
@@ -1,1 +1,11 @@
 from django.shortcuts import render
+from django.db.models import Q, F
+
+from store import models
+
+
+def show_data(request):
+    quesry_set = models.OrderItem.objects.values_list('product').distinct()
+    print(list(quesry_set))
+    products = models.Product.objects.filter(id__in=quesry_set)
+    return render(request, 'store/index.html', {'products': list(products)})
