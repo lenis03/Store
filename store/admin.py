493c51962e268c71a241bf60ef0ab1c8c2188cef
lenis <fardintorkamand83@gmail.com>
2023-10-28 17:48:12 +0330

    Create product model admin

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from store.models import Category, Order, Product
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'inventory', 'unit_price']
+    list_per_page = 10
+    list_editable = ['unit_price']
+
