93437d745ba70dc32f2cf437de5141b189303cbd
lenis <fardintorkamand83@gmail.com>
2023-10-28 17:46:07 +0330

    Implement  str method customer

--- store/models.py
+++ store/models.py
@@ -42,6 +42,9 @@ class Customer(models.Model):
     phone_number = models.CharField(max_length=255, unique=True)
     birth_date = models.DateField(null=True, blank=True)
 
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
 
 class Address(models.Model):
     customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
