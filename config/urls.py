56138c0356793bdc84806d58229dc115a448d630
lenis <fardintorkamand83@gmail.com>
2023-10-28 12:04:00 +0330

    Change header admin panel & index title

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = 'Store'
+admin.site.index_title = 'Special Access'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('store/', include('store.urls', namespace='store')),
