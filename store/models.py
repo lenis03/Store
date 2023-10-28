df9342d30a387e956d0b305bddc547b8757a768a
lenis <fardintorkamand83@gmail.com>
2023-10-28 17:08:43 +0330

    Implement str for category

--- store/models.py
+++ store/models.py
@@ -10,6 +10,9 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'category'
         verbose_name_plural = 'categorise'
+        
+    def __str__(self):
+        return self.title
 
 
 class Discount(models.Model):
